8104bd3107c50872914f7211a41516237b9b8c3e
warp5t <dmitrij.didencko@yandex.by>
2023-04-27 14:02:44 +0300

    feat: fill subscribes buttons

--- js.js
+++ js.js
@@ -2,7 +2,27 @@ const keyboard = {
   body: document.getElementById('body'),
   bodyKeyboard: document.createElement('div'),
   display: document.createElement('div'),
-  ammountsBtn: 65,
+  ammountsBtn: 64,
+  subscribeBtn_1: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspase',
+    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+    'Caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
+    'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑',
+    'Shift', 'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→'],
+  subscribeBtn_2: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspase',
+    'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del',
+    'Caps lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter',
+    'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑',
+    'Shift', 'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→'],
+  subscribeBtn_3: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspase',
+    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del',
+    'Caps lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+    'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', '↑',
+    'Shift', 'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→'],
+  subscribeBtn_4: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspase',
+    'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Del',
+    'Caps lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter',
+    'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/', '↑',
+    'Shift', 'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→'],
 };
 
 function bodyCreating() {
@@ -37,13 +57,19 @@ function btnFilling() {
       buttons[56].classList.add('btn-57');
     } else if (i === 56) {
       buttons[55].classList.add('btn-56');
-    } else if (i === 60) {
-      buttons[59].classList.add('btn-60');
+    } else if (i === 55) {
+      buttons[54].classList.add('btn-55');
+    } else if (i === 59) {
+      buttons[58].classList.add('btn-60');
     }
   }
 }
 btnFilling();
 
-function btnSubscribing(){
-
+function btnSubscribing() {
+  const buttons = document.querySelectorAll('.button-keyboard');
+  for (let i = 0; keyboard.ammountsBtn > i; i += 1) {
+    buttons[i].innerText = keyboard.subscribeBtn_1[i];
+  }
 }
+btnSubscribing();
